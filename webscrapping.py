43afd1dcb1a590a476085c8617d8c5f95898ea30
Francisco <fvarelacid@gmail.com>
2021-06-21 18:26:44 +0100

    New Webscrapping File

---
+++ webscrapping.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup
+import requests
+
+
+class1 = "display--inline__09f24__EhyFv border-color--default__09f24__1eOdn"
+
+class2 = "display--inline__09f24__EhyFv border-color--default__09f24__1eOdn"
+
+assert class1 == class2
+
+url = "https://www.yelp.co.uk/search?find_desc=&find_loc=Berlin%2C%20Germany&ns=1&start=0"
+
