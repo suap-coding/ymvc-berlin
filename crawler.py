272a908a8a66dd67800009b7e6fc765555d9aa64
Rajat Sehgal <sehgal.rajat1007@gmail.com>
2021-06-23 14:00:03 +0200

    Add partially working crawler script

--- crawler.py
+++ crawler.py
@@ -4,15 +4,25 @@ from bs4 import BeautifulSoup
 import requests
 import time
 import re
+from tqdm import tqdm
 
+df = pd.read_csv('yelp_dataset.csv')
+total_webpages = range(0, df['url'].shape[0])
+n = 3
+sessions = [total_webpages[i:i+n] for i in range(0, len(total_webpages), n)]
 
-business_pages = []
-index = 0
-response = requests.get(df['url'][index])
-delay = np.random.randint(20,40)
-time.sleep(delay)
-soup = BeautifulSoup(response.content, 'html.parser')
-business_pages.append(soup)
+for session, session_range in tqdm(enumerate(sessions)):
 
-with open(f"business_page{index}.html", "w") as file:
-    file.write(str(soup))
+    for index in tqdm(session_range):
+
+        # print(f"\nSession {session}", "Index: ", index)
+        response = requests.get(df['url'][index])
+        delay = np.random.randint(25,50)
+        time.sleep(delay)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        with open(f"./webpages/business_page{index}.html", "w") as file:
+            file.write(str(soup))
+    
+    time.sleep(np.random.randint(600,800))
+    # print(f"\nSession {session} completed")
