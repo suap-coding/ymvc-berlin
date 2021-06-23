955e2bde641014c252c687572006c92512ac87c8
Rajat Sehgal <sehgal.rajat1007@gmail.com>
2021-06-23 15:25:43 +0200

    Update crawler to work with all webpages
    However, it doesn't load everything properly. Maybe it will work by reading with unicode

--- crawler.py
+++ crawler.py
@@ -6,23 +6,26 @@ import time
 import re
 from tqdm import tqdm
 
+# Reference: https://jasonfavrod.com/writing-web-scraped-html-to-a-file/
+
 df = pd.read_csv('yelp_dataset.csv')
 total_webpages = range(0, df['url'].shape[0])
 n = 3
-sessions = [total_webpages[i:i+n] for i in range(0, len(total_webpages), n)]
+sessions = [total_webpages[i:i+n] for i in range(0, len(total_webpages)+1, n)]
 
+# print(sessions)
 for session, session_range in tqdm(enumerate(sessions)):
-
+    # print(session)
     for index in tqdm(session_range):
-
         # print(f"\nSession {session}", "Index: ", index)
         response = requests.get(df['url'][index])
         delay = np.random.randint(25,50)
         time.sleep(delay)
-        soup = BeautifulSoup(response.content, 'html.parser')
+        html = response.text
 
         with open(f"./webpages/business_page{index}.html", "w") as file:
-            file.write(str(soup))
+            page = bytes(html, 'utf-8')
+            file.write(str(page))
     
     time.sleep(np.random.randint(600,800))
     # print(f"\nSession {session} completed")
